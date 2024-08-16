10fc5ee6ee88dbd93162208509fbb8faccac51a8
Ryan <ryanshaunsmith@gmail.com>
2024-08-16 08:29:34 +0100

    ported over initial html and styling

--- learning-audit-app/app/page.js
+++ learning-audit-app/app/page.js
@@ -4,92 +4,60 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
+      <section className={styles.contentInput}>
+        <div className={styles.wide}>
+          <form className={styles.siteForm} action="" method="post">
+            <label for="course-module">Add a new course Module:</label>
+            <input
+              type="text"
+              name="course-module"
+              id="course-module"
+              placeholder="Module name..."
             />
-          </a>
+            <button type="submit">ADD</button>
+          </form>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        <div className={styles.wide}>
+          <form className={styles.siteForm} action="" method="post">
+            <label for="learning">What did I learn?</label>
+            <input
+              type="text"
+              name="learning"
+              id="learning"
+              placeholder="Type here..."
+            />
+            <select name="linked-course" id="linked-course">
+              <option value="" disabled selected>
+                In which course?
+              </option>
+              <option value="dummy">dummy</option>
+            </select>
+            <select name="rag" id="rag">
+              <option value="red">Red 🔴</option>
+              <option value="amber">Amber 🟠</option>
+              <option value="green">Green 🟢</option>
+            </select>
+            <textarea
+              rows="3"
+              name="learning-notes"
+              id="learning-notes"
+              placeholder="Thoughts worthy of noting in terms of my learning..."
+            ></textarea>
+            <button type="submit">ADD</button>
+          </form>
+        </div>
+      </section>
+      <section className={styles.display}>
+        <div className={styles.moduleDisplay}>
+          <h3>Onboarding</h3>
+          <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
+      </section>
     </main>
   );
 }
