925ea619574dc0a147e050cf18302844830c2869
Ryan <ryanshaunsmith@gmail.com>
2024-08-18 11:26:03 +0100

    added some code to access the server api

--- learning-audit-app/app/page.js
+++ learning-audit-app/app/page.js
@@ -1,52 +1,25 @@
+"use client";
+
 import Image from "next/image";
 import styles from "./page.module.css";
+import { useState, useEffect } from "react";
+import FormsSection from "./components/FormsSections/FormsSection";
 
 export default function Home() {
+  const [message, setMessage] = useState([]);
+
+  const api = async function () {
+    const response = await fetch("http://localhost:3010/api/modules");
+    const data = await response.json();
+    const { payload } = data;
+    setMessage(payload);
+  };
+
+  api();
+
   return (
     <main className={styles.main}>
-      <section className={styles.contentInput}>
-        <div className={styles.wide}>
-          <form className={styles.siteForm} action="" method="post">
-            <label htmlFor="course-module">Add a new course Module:</label>
-            <input
-              type="text"
-              name="course-module"
-              id="course-module"
-              placeholder="Module name..."
-            />
-            <button type="submit">ADD</button>
-          </form>
-        </div>
-        <div className={styles.wide}>
-          <form className={styles.siteForm} action="" method="post">
-            <label htmlFor="learning">What did I learn?</label>
-            <input
-              type="text"
-              name="learning"
-              id="learning"
-              placeholder="Type here..."
-            />
-            <select name="linked-course" id="linked-course" defaultValue={0}>
-              <option value="default" disabled>
-                In which course?
-              </option>
-              <option value="dummy">dummy</option>
-            </select>
-            <select name="rag" id="rag">
-              <option value="red">Red 🔴</option>
-              <option value="amber">Amber 🟠</option>
-              <option value="green">Green 🟢</option>
-            </select>
-            <textarea
-              rows="3"
-              name="learning-notes"
-              id="learning-notes"
-              placeholder="Thoughts worthy of noting in terms of my learning..."
-            ></textarea>
-            <button type="submit">ADD</button>
-          </form>
-        </div>
-      </section>
+      <FormsSection />
       <section className={styles.display}>
         <div className={styles.moduleDisplay}>
           <h3>Onboarding</h3>
