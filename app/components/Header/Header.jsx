83ad73322603e776a36c80ae0b59769ab7f6d280
Ryan <ryanshaunsmith@gmail.com>
2024-08-29 20:23:18 +0100

    Add logo and restyle Headeer

--- app/components/Header/Header.jsx
+++ app/components/Header/Header.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 import Image from "next/image";
-import styles from './Header.module.css';
+import styles from "./Header.module.css";
 
 import books from "../../../public/images/books.png";
+import logo from "../../../public/images/logo.png";
 
 export default function Header() {
-    return(
-        <header className={styles.siteHeader}>
-            The Learning Audit <Image className={styles.siteHeaderImage} src={books} alt="stacked books" width={512} height={512} />
-        </header>
-    )
+  return (
+    <header className={styles.siteHeader}>
+      <Image
+        className={styles.siteHeaderImage}
+        src={logo}
+        alt="stacked books"
+        width={512}
+        height={512}
+      />
+      <h1 className={styles.siteHeading}>
+        The
+        <br />
+        Learning Audit
+      </h1>
+    </header>
+  );
 }
