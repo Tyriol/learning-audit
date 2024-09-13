2f5c1915109247378101906d99613a940c124af6
Ryan <ryanshaunsmith@gmail.com>
2024-09-13 17:51:48 +0100

    add event to module listing button

--- app/components/ModuleList/ModuleList.jsx
+++ app/components/ModuleList/ModuleList.jsx
@@ -1,6 +1,16 @@
 import styles from "./ModuleList.module.css";
 
 export default function ModuleList({ moduleList, loading }) {
+  // on button click
+  // it take module id
+  // and makes a get request to get all learnings associated with that module id
+  // opens a modal***
+  // displays the module title and a list of learnings in
+
+  function handleClick(e) {
+    console.log(e);
+  }
+
   return (
     <section className={styles.display}>
       {loading ? (
@@ -10,7 +20,12 @@ export default function ModuleList({ moduleList, loading }) {
           {moduleList.map((module) => {
             return (
               <li key={module.id} className={styles.uListItem}>
-                <button className={styles.button} type="button">
+                <button
+                  onClick={(e) => handleClick(e)}
+                  className={styles.button}
+                  type="button"
+                  id={"module" + module.id}
+                >
                   <h3>{module.module_name}</h3>
                 </button>
               </li>
