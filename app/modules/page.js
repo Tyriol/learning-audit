1d4c2788264262b10ee3c63ab09357ca0af42d53
Ryan <ryanshaunsmith@gmail.com>
2024-11-27 21:26:46 +0000

    add module list to module page

--- app/modules/page.js
+++ app/modules/page.js
@@ -1,3 +1,49 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
+import ModuleList from "../components/ModuleList/ModuleList";
+import SiteNavigationButton from "../components/SiteNavigationButton/SiteNavigationButton";
+
 export default function Modules() {
-  return <h1>HI!</h1>;
+  const [moduleList, setModuleList] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [isOpen, setIsOpen] = useState(false);
+
+  // Fetch modules and set the modulelist state for use elsewhere, or the loading state or error state
+  useEffect(() => {
+    const fetchModules = async () => {
+      try {
+        const response = await fetch(
+          "https://learning-audit.onrender.com/api/modules"
+        );
+        if (!response.ok) {
+          throw new Error(`HTTP error: Status ${response.status}`);
+        }
+        let moduleData = await response.json();
+        setModuleList(moduleData.payload);
+        setError(null);
+      } catch (err) {
+        setError(err.message);
+        console.log(error);
+        setModuleList(null);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchModules();
+  }, []);
+
+  return (
+    <>
+      <ModuleList
+        moduleList={moduleList}
+        loading={loading}
+        isOpen={isOpen}
+        setIsOpen={setIsOpen}
+      />
+      <SiteNavigationButton title="Home Page" link="/" />
+    </>
+  );
 }
