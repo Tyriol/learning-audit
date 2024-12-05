29f18812a9e096c836ee4288a69d8dbb674956d8
Ryan <ryanshaunsmith@gmail.com>
2024-12-05 22:01:28 +0000

    add sign in route wip

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,13 @@
 import express from "express";
 import bcrypt from "bcrypt";
+
 import { pool } from "../db/index.js";
+import {
+  createAccessToken,
+  createRefreshToken,
+  sendAccessToken,
+  sendRefreshToken,
+} from "../utils/tokens.js";
 
 const router = express.Router();
 
@@ -45,6 +52,59 @@ router.post("/signup", async (req, res) => {
       type: "error",
       message: "Error creating user!",
       error: error.message,
+    });
+  }
+});
+
+router.post("/signin", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    console.log(email, password);
+
+    const findUserQuery = "SELECT * FROM users WHERE email = $1";
+    const user = await pool.query(findUserQuery, [email]);
+    console.log("User", user.rows);
+    if (user.rows.length === 0)
+      return res.status(500).json({
+        message: "User doesn't exist! 😢 Try signing in.",
+        type: "error",
+      });
+
+    const isPassMatch = await bcrypt.compare(password, user.rows[0].password);
+    console.log("Is pass Match?", isPassMatch);
+    if (!isPassMatch)
+      return res.status(500).json({
+        message: "Password is incorrect! ⚠️",
+        type: "error",
+      });
+
+    const accessToken = createAccessToken(user.rows._id);
+    console.log(accessToken);
+    const refreshToken = createRefreshToken(user.rows._id);
+    // add to db
+    const refreshTokenQuery = ` UPDATE users 
+                                SET refresh_token = $1
+                                WHERE id = $2
+                                RETURNING *`;
+    console.log("refresh", refreshToken);
+    try {
+      const addRefreshToken = await pool.query(refreshTokenQuery, [
+        refreshToken,
+        user.rows.id,
+      ]);
+      console.log("addrefresh", addRefreshToken.rows[0]);
+      if (addRefreshToken.rows === 1) {
+        sendRefreshToken(res, refreshToken);
+        sendAccessToken(req, res, accessToken);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  } catch (error) {
+    res.status(500).json({
+      type: "error",
+      message: "Error signing in!",
+      error,
     });
   }
 });
