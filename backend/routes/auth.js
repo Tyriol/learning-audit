ea38387f770ba639306854e981e52991fc28f164
Ryan <ryanshaunsmith@gmail.com>
2024-12-06 21:10:04 +0000

    remove console logs

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -72,17 +72,14 @@ router.post("/signin", async (req, res) => {
       });
 
     const accessToken = createAccessToken(user.rows[0].id);
-    console.log(accessToken);
     const refreshToken = createRefreshToken(user.rows[0].id);
     // add to db
     const refreshTokenQuery = ` UPDATE users 
                                 SET refresh_token = $1
                                 WHERE id = $2
                                 RETURNING *`;
-    console.log("refresh", refreshToken);
     // try {
     const addRefreshToken = await pool.query(refreshTokenQuery, [refreshToken, user.rows[0].id]);
-    console.log("addrefresh", addRefreshToken.rows);
     if (addRefreshToken.rows.length === 1) {
       sendRefreshToken(res, refreshToken);
       sendAccessToken(req, res, accessToken);
