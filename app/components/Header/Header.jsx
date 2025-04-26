b883b9239ecd5071d30249001de1b5c81a32c33a
Ryan <ryanshaunsmith@gmail.com>
2025-04-26 19:46:46 +0100

    add links to navigate through site while working on it

--- app/components/Header/Header.jsx
+++ app/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 import styles from "./Header.module.css";
 
 import logo from "../../../public/images/logo.png";
@@ -7,18 +8,39 @@ import logo from "../../../public/images/logo.png";
 export default function Header() {
   return (
     <header className={styles.siteHeader}>
-      <Image
-        className={styles.siteHeaderImage}
-        src={logo}
-        alt="stacked books"
-        width={512}
-        height={512}
-      />
-      <h1 className={styles.siteHeading}>
-        The
-        <br />
-        Learning Audit
-      </h1>
+      <div className={styles.logoTitle}>
+        <Image
+          className={styles.siteHeaderImage}
+          src={logo}
+          alt="stacked books"
+          width={512}
+          height={512}
+        />
+        <h1 className={styles.siteHeading}>
+          The
+          <br />
+          Learning Audit
+        </h1>
+      </div>
+      <nav>
+        <ul className={styles.nav}>
+          <li>
+            <Link href="/" className={styles.navLink}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link href="/modules" className={styles.navLink}>
+              Modules
+            </Link>
+          </li>
+          <li>
+            <Link href="/auth" className={styles.navLink}>
+              Auth Page
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }
