e541e92681166b344e56770429bd9c4c59456a61
Ryan <ryanshaunsmith@gmail.com>
2025-04-30 19:46:05 +0100

    WIP add logic to connect to auth api and test using sign in route

--- app/auth/page.js
+++ app/auth/page.js
@@ -17,29 +17,124 @@ import { useRouter } from "next/navigation";
 // password
 // forgot password form should have
 // email
+const handleSubmit = async (formView, email, username, password) => {
+  try {
+    let endPoint = "";
+    let payload = {};
+
+    switch (formView) {
+      case "signin":
+        (endPoint = "/auth/signin"),
+          (payload = {
+            email: email,
+            password: password,
+          });
+        break;
+      case "signup":
+        (endPoint = "/auth/signup"),
+          (payload = {
+            email: email,
+            username: username,
+            password: password,
+          });
+        break;
+      case "resetPassword":
+        (endPoint = "/auth/reset-password"),
+          (payload = {
+            email: email,
+          });
+        break;
+    }
+
+    const response = await fetch(
+      process.env.NODE_ENV === "production"
+        ? `https://learning-audit.onrender.com${endPoint}`
+        : `http://localhost:3010${endPoint}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(payload),
+      }
+    );
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    return {
+      type: "error",
+      message: "Error submiting the form",
+      error,
+    };
+  }
+};
+
 export default function Auth() {
   const router = useRouter();
   const [formView, setFormView] = useState("signin");
+  const [state, submitAction, isPending] = useActionState(async (prev, formData) => {
+    const email = formData.get("email");
+    const username = formData.get("username");
+    const password = formData.get("password");
+
+    //
+    if (
+      !email ||
+      (formView === "signup" && !username) ||
+      (formView !== "resetPassword" && !password)
+    ) {
+      return {
+        type: "error",
+        message: "All fields are required",
+      };
+    } else if (!/\S+@\S+\.\S+/.test(email)) {
+      return {
+        type: "error",
+        message: "The email address is invalid",
+      };
+    }
+
+    const response = await handleSubmit(formView, email, username, password);
+    console.log("state", state);
+
+    return {
+      response,
+    };
+  });
+
+  const toggleFormView = () => {
+    setFormView(formView === "signin" ? "signup" : "signin");
+    ReactDOM.requestFormReset(formData);
+  };
+
   return (
     <div className={styles.container}>
       <h2>{formView === "signin" ? "Sign In" : "Sign Up"}</h2>
-      <form>
-        <label className={styles.formInput}>
+      <form action={submitAction}>
+        <label htmlFor="email" className={styles.formInput}>
           Email Address:
-          <input type="email"></input>
         </label>
-        {formView === "signin" ? (
-          <label className={styles.formInput}>
-            Username:
-            <input type="text"></input>
-          </label>
+        <input id="email" name="email" type="email"></input>
+        {formView === "signup" ? (
+          <>
+            <label htmlFor="username" className={styles.formInput}>
+              Username:
+            </label>
+            <input id="username" name="username" type="text"></input>
+          </>
         ) : null}
-        <label className={styles.formInput}>
+        <label htmlFor="password" className={styles.formInput}>
           Password:
-          <input type="password"></input>
         </label>
+        <input id="password" name="password" type="password"></input>
+        <button type="submit">{formView === "signin" ? "Sign In" : "Sign Up"}</button>
       </form>
-      <p>No Account? Sign In instead</p>
+      {state && <p>{state.response.message}</p>}
+      <button onClick={toggleFormView}>
+        {formView === "signin"
+          ? "Don't have an account? Sign up instead"
+          : "Already have an account? Sign in instead"}
+      </button>
     </div>
   );
 }
