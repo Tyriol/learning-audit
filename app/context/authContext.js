601f3d2915512fed83fe0158be3b7b7fc2fcee4a
Ryan <ryanshaunsmith@gmail.com>
2025-05-04 15:22:10 +0100

    add useEffect on auth context to check authentication and add basic check to see if access token exists

--- app/context/authContext.js
+++ app/context/authContext.js
@@ -1,11 +1,35 @@
 "use client";
 
-import { useState, createContext } from "react";
+import { useState, useEffect, createContext } from "react";
 
 export const AuthContext = createContext();
 
 export function AuthProvider({ children }) {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    checkAuth();
+  }, []);
+
+  const checkAuth = async () => {
+    try {
+      const accessToken = localStorage.getItem("accesstoken");
+      if (!accessToken) {
+        setIsAuthenticated(false);
+        setUser(null);
+        return;
+      }
+      console.log("access token:", accessToken);
+      setUser("test");
+    } catch (error) {
+      console.error("Authentication check failed:", error);
+      setIsAuthenticated(false);
+      setUser(null);
+    }
+  };
+
   const value = { isAuthenticated, setIsAuthenticated };
+
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
