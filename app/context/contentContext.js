58ed659aef7c7804933d9201a2039a61846a97c0
Ryan <ryanshaunsmith@gmail.com>
2025-05-09 12:29:46 +0100

    create contentContext and wrap app in the provider

---
+++ app/context/contentContext.js
@@ -0,0 +1,62 @@
+"use client";
+
+import { useState, useEffect, useContext, createContext } from "react";
+import { AuthContext } from "./authContext";
+
+export const ContentContext = createContext();
+
+export function ContentProvider({ children }) {
+  const { isAuthenticated, refreshToken } = useContext(AuthContext);
+  const [moduleData, setModuleData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  //   const [learningsData, setLearningsData] = useState(null);
+
+  useEffect(() => {
+    fetchAllData();
+  }, []);
+
+  const fetchAllData = async () => {
+    console.log("hello, lets start");
+
+    // if (!isAuthenticated) return false;
+    try {
+      console.log("fetching access token");
+
+      let accessToken = localStorage.getItem("accesstoken");
+      if (!accessToken) {
+        await refreshToken();
+        accessToken = localStorage.getItem("accesstoken");
+      }
+      const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/modules`, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+        credentials: "include",
+      });
+      if (!response.ok) {
+        throw new Error(`HTTP error: Status ${response.status}`);
+      }
+      let data = await response.json();
+      setModuleData(data.payload);
+      setError(null);
+    } catch (err) {
+      setError(err.message);
+      console.error(err);
+      setModuleData(null);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const value = {
+    moduleData,
+    setModuleData,
+    fetchAllData,
+    loading,
+    error,
+  };
+
+  return <ContentContext.Provider value={value}>{children}</ContentContext.Provider>;
+}
