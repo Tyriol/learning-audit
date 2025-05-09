46927fc9e7895e3849a4ec8be3815fdefaa4be07
Ryan <ryanshaunsmith@gmail.com>
2025-05-09 12:30:31 +0100

    refactor home page and module page to get data from the content context

--- app/page.js
+++ app/page.js
@@ -1,56 +1,24 @@
 "use client";
 
 import styles from "./page.module.css";
-import { useState, useEffect, useContext } from "react";
+import { useContext, useEffect } from "react";
 
-import { AuthContext } from "./context/authContext";
+import { ContentContext } from "./context/contentContext";
 
 import ProtectedRoute from "./routes/ProtectedRoute";
 import FormsSection from "./components/FormsSections/FormsSection";
 
 export default function Home() {
-  const { refreshToken } = useContext(AuthContext);
-  const [moduleList, setModuleList] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+  const { fetchAllData, moduleData, setModuleData, loading } = useContext(ContentContext);
 
-  // Fetch modules and set the modulelist state for use elsewhere, or the loading state or error state
   useEffect(() => {
-    const fetchModules = async () => {
-      try {
-        let accessToken = localStorage.getItem("accesstoken");
-        if (!accessToken) {
-          await refreshToken();
-          accessToken = localStorage.getItem("accesstoken");
-        }
-        const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/modules`, {
-          method: "GET",
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-          },
-          credentials: "include",
-        });
-        if (!response.ok) {
-          throw new Error(`HTTP error: Status ${response.status}`);
-        }
-        let moduleData = await response.json();
-        setModuleList(moduleData.payload);
-        setError(null);
-      } catch (err) {
-        setError(err.message);
-        console.error(err);
-        setModuleList(null);
-      } finally {
-        setLoading(false);
-      }
-    };
-    fetchModules();
+    fetchAllData();
   }, []);
 
   return (
     <main className={styles.main}>
       <ProtectedRoute>
-        <FormsSection moduleList={moduleList} setModuleList={setModuleList} loading={loading} />
+        <FormsSection moduleList={moduleData} setModuleList={setModuleData} loading={loading} />
       </ProtectedRoute>
     </main>
   );
