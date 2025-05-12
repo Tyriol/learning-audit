1767011844fbf0c0f57f318a6e9202c0d52f456e
Ryan <ryanshaunsmith@gmail.com>
2025-05-12 20:37:20 +0100

    clean up components now that context is being used

--- app/components/FormsSections/FormsSection.jsx
+++ app/components/FormsSections/FormsSection.jsx
@@ -3,11 +3,11 @@ import styles from "./FormsSection.module.css";
 import NewModuleForm from "./NewModuleForm/NewModuleForm";
 import NewLearningForm from "./NewLearningForm/NewLearningForm";
 
-export default function FormsSection({ moduleList, setModuleList, loading }) {
+export default function FormsSection() {
   return (
     <section className={styles.contentInput}>
-      <NewModuleForm moduleList={moduleList} setModuleList={setModuleList} />
-      <NewLearningForm moduleList={moduleList} loading={loading} />
+      <NewModuleForm />
+      <NewLearningForm />
     </section>
   );
 }
