2cae905fcae1c25b9f35eddd5f9897a488e1d67d
Ryan <ryanshaunsmith@gmail.com>
2025-05-12 20:30:38 +0100

    refactor new Module form to use useActionState and include error and load state handling

--- app/components/FormsSections/NewModuleForm/NewModuleForm.jsx
+++ app/components/FormsSections/NewModuleForm/NewModuleForm.jsx
@@ -1,35 +1,47 @@
-import React from "react";
+import { useContext, useActionState } from "react";
 import styles from "../NewForm.module.css";
 import { AuthContext } from "@/app/context/authContext";
+import { ContentContext } from "@/app/context/contentContext";
 
-export default function NewModuleForm({ setModuleList }) {
-  const { user } = React.useContext(AuthContext);
-  // handle submit of new module data to update db and re-render module list
-  async function handleNewModuleSubmit(event) {
-    event.preventDefault();
-    const form = event.target;
-    const formData = new FormData(form);
-    const formJson = Object.fromEntries(formData.entries());
+export default function NewModuleForm() {
+  const { user } = useContext(AuthContext);
+  const { setModuleData } = useContext(ContentContext);
 
-    // post new module
-    const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/modules`, {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ ...formJson, userId: user.id }),
-    });
-    const jsonResponse = await response.json();
-    const newModule = jsonResponse.payload;
-    setModuleList((moduleList) => [...moduleList, newModule]);
+  const [state, submitNewModule, isPending] = useActionState(async (prev, formData) => {
+    const moduleName = formData.get("moduleName");
+    const description = formData.get("description");
+    try {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/modules`, {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ moduleName, description, userId: user.id }),
+      });
+      if (!response.ok) {
+        return {
+          type: "error",
+          message: "there was an error while creating your new learning entry",
+        };
+      }
+      const jsonResponse = await response.json();
+      const newModule = jsonResponse.payload;
+      setModuleData((prevModuleData) => [...prevModuleData, newModule]);
 
-    alert(`New module added: ${JSON.stringify(newModule.module_name)}`);
-  }
+      alert(`New module added: ${JSON.stringify(newModule.module_name)}`);
+    } catch (err) {
+      console.error(err);
+      return {
+        type: "error",
+        message: "An unexpected error occurred. Please try again.",
+      };
+    }
+  });
 
   return (
     <div className={styles.wide}>
-      <form className={styles.siteForm} onSubmit={handleNewModuleSubmit} method="post">
+      <form className={styles.siteForm} action={submitNewModule}>
         <label className={styles.formLabel} htmlFor="moduleName">
           Add a new course Module:
           <input
@@ -50,8 +62,9 @@ export default function NewModuleForm({ setModuleList }) {
             placeholder="Description of the module..."
           />
         </label>
+        {state ? <p className="error">There was an error adding your new learning</p> : null}
         <button className={styles.button} type="submit">
-          ADD
+          {isPending ? "Adding your new module" : "ADD"}
         </button>
       </form>
     </div>
