a111391da2b570abfe772f69c62974a536d38b3f
Ryan <ryanshaunsmith@gmail.com>
2025-05-20 17:53:44 +0100

    add component styling for learning items

--- app/components/LearningList/LearningList.jsx
+++ app/components/LearningList/LearningList.jsx
@@ -1,11 +1,15 @@
 import { useContext } from "react";
-
+import styles from "./LearningList.module.css";
 import { ContentContext } from "@/app/context/contentContext";
 
 export default function LearningList() {
   const { learningData } = useContext(ContentContext);
   const learningList = learningData.map((learning) => {
-    return <li key={learning.id}>{learning.learning_name}</li>;
+    return (
+      <li className={styles.listItem} key={learning.id}>
+        {learning.learning_name}
+      </li>
+    );
   });
-  return <ul>{learningList}</ul>;
+  return <ul className={styles.list}>{learningList}</ul>;
 }
