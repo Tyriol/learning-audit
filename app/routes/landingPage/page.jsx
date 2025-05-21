66a52f73d160d772ea3208abc16f8a9da46f40af
Ryan <ryanshaunsmith@gmail.com>
2025-05-21 16:47:33 +0100

    add landing page with basic styling

---
+++ app/routes/landingPage/page.jsx
@@ -0,0 +1,19 @@
+import styles from "./landingPage.module.css";
+
+export default function LandingPage() {
+  return (
+    <div className={styles.container}>
+      <section className={styles.heroMessage}>
+        <h1>OWN YOUR LEARNING JOURNEY</h1>
+        <p className={styles.description}>
+          Learning fast doesn’t mean rushing. <br />
+          Pause. Reflect. Plan. Act.
+          <br />
+          Take control of your progress with intention.
+        </p>
+        <button>Start your Learning Audit now</button>
+      </section>
+      <section className={styles.screenshots}></section>
+    </div>
+  );
+}
