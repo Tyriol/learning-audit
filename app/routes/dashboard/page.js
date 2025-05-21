d7d4b70d725c7169cef328c4d145190b6818a7e0
Ryan <ryanshaunsmith@gmail.com>
2025-05-21 09:51:30 +0100

    abstract intro section into its own client component

--- app/routes/dashboard/page.js
+++ app/routes/dashboard/page.js
@@ -1,23 +1,16 @@
-"use client";
-
 import styles from "./page.module.css";
-import { useContext } from "react";
 import ProtectedRoute from "../ProtectedRoute";
-import { AuthContext } from "@/app/context/authContext";
 
+import Intro from "@/app/components/Intro/Intro";
 import ModuleList from "@/app/components/ModuleList/ModuleList";
 import LearningList from "@/app/components/LearningList/LearningList";
 
 export default function Dashboard() {
-  const { user } = useContext(AuthContext);
   return (
     <ProtectedRoute>
       <div className={styles.container}>
         <div className={styles.info}>
-          <h2>Hey again {user.username}</h2>
-          <p>What are you going to focus on today</p>
-          <p>Or would you like to...</p>
-          <button>Add a new module</button>
+          <Intro />
         </div>
         <div className={styles.learnings}>
           <h3>Your Learnings</h3>
