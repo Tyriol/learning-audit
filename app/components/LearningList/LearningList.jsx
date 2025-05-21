cf20ee50b881813de3cccfba67b9e15106dd644d
Ryan <ryanshaunsmith@gmail.com>
2025-05-21 09:52:18 +0100

    add loading logic to the learning list and add client call

--- app/components/LearningList/LearningList.jsx
+++ app/components/LearningList/LearningList.jsx
@@ -1,11 +1,15 @@
+"use client";
+
 import { useContext } from "react";
 import styles from "./LearningList.module.css";
 import { ContentContext } from "@/app/context/contentContext";
 
 export default function LearningList() {
-  const { learningData } = useContext(ContentContext);
+  const { learningData, loading } = useContext(ContentContext);
   const learningList = learningData.map((learning) => {
-    return (
+    return loading ? (
+      <p>Loading</p>
+    ) : (
       <li className={styles.listItem} key={learning.id}>
         {learning.learning_name}
       </li>
