1a3c088a1e71ad93209cf3af7f896afe1a9c7836
Ryan <ryanshaunsmith@gmail.com>
2025-05-22 19:34:21 +0100

    add modal to display add learning form

--- app/components/ModuleLearnings/ModuleLearnings.jsx
+++ app/components/ModuleLearnings/ModuleLearnings.jsx
@@ -1,21 +1,34 @@
 "use client";
 
 import styles from "./ModuleLearning.module.css";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { ContentContext } from "@/app/context/contentContext";
+import Modal from "../Modal/Modal";
+import NewLearningForm from "../FormsSections/NewLearningForm/NewLearningForm";
 
 export default function ModuleLearnings({ moduleId }) {
   const { moduleData, learningData } = useContext(ContentContext);
+  const [isOpen, setIsOpen] = useState(false);
   const module = moduleData.find((module) => module.id === moduleId);
   const moduleLearningsArray = learningData.filter((learning) => learning.module_id === moduleId);
   const learnings = moduleLearningsArray.map((learning) => {
     return <p key={learning.id}>{learning.learning_name}</p>;
   });
 
+  const handleClick = () => {
+    setIsOpen(true);
+  };
+
   return (
     <section className={styles.container}>
       <h2>{module.module_name}</h2>
       <p>{module.description}</p>
+      <button type="button" onClick={handleClick}>
+        Add a new learning
+      </button>
+      <Modal open={isOpen} onClose={() => setIsOpen(false)} title="Add a new learning">
+        <NewLearningForm />
+      </Modal>
       <div>{learnings}</div>
     </section>
   );
