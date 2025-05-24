41702796b4d87c2bdf840105145355ea76591183
Ryan <ryanshaunsmith@gmail.com>
2025-05-24 10:21:15 +0100

    add basic learning li component so it's reusable

---
+++ app/components/Learning/Learning.jsx
@@ -0,0 +1,15 @@
+import styles from "./Learning.module.css";
+
+export default function Learning({ learning }) {
+  const { id, rag_status: ragStatus, learning_name: name, description } = learning;
+
+  const colour =
+    ragStatus === "red" ? styles.red : ragStatus === "amber" ? styles.amber : styles.green;
+  const listItemClass = `${styles.listItem} ${colour}`;
+
+  return (
+    <li key={id} className={listItemClass}>
+      {name}
+    </li>
+  );
+}
