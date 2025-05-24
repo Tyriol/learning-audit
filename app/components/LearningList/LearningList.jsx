4483fd647b42fd540365e51df76579dc1d8fdc07
Ryan <ryanshaunsmith@gmail.com>
2025-05-24 10:44:51 +0100

    add dynamic colouring to the learnings

--- app/components/LearningList/LearningList.jsx
+++ app/components/LearningList/LearningList.jsx
@@ -2,27 +2,18 @@
 
 import { useContext } from "react";
 import styles from "./LearningList.module.css";
+import Learning from "../Learning/Learning";
 import { ContentContext } from "@/app/context/contentContext";
 
 export default function LearningList() {
   const { learningData, loading } = useContext(ContentContext);
-  console.log(learningData);
   const learningList = learningData.map((learning) => {
-    const colour =
-      learning.rag_status === "red"
-        ? styles.red
-        : learning.rag_status === "amber"
-        ? styles.amber
-        : styles.green;
-    const listItemClass = `${styles.listItem} ${colour}`;
     return loading ? (
       <p>Loading</p>
     ) : learningData.length === 0 ? (
       <p>Add some learnings to see them here</p>
     ) : (
-      <li className={listItemClass} key={learning.id}>
-        {learning.learning_name}
-      </li>
+      <Learning learning={learning} />
     );
   });
   return <ul className={styles.list}>{learningList}</ul>;
