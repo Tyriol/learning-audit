450ac555a0bc50d72da36aa236cf5fd3c842dcbc
Ryan <ryanshaunsmith@gmail.com>
2025-05-27 10:36:11 +0100

    update modal to check for null dialogRef and include an aria-labelledby for accessibility

--- app/components/Modal/Modal.jsx
+++ app/components/Modal/Modal.jsx
@@ -4,20 +4,26 @@ import styles from "./Modal.module.css";
 
 import { useRef } from "react";
 
-export default function ModalDialogue({ openButtonText, closeButtonText, children }) {
+export default function ModalDialogue({ title, openButtonText, closeButtonText, children }) {
   const dialogRef = useRef(null);
 
   const showModal = () => {
-    dialogRef.current.showModal();
+    if (dialogRef.current) {
+      dialogRef.current.showModal();
+    } else {
+      console.warn("The dialogRef is not set, unable to open the modal");
+    }
   };
+
   return (
     <>
       <button onClick={showModal} type="button">
         {openButtonText}
       </button>
 
-      <dialog className={styles.modal} ref={dialogRef}>
+      <dialog className={styles.modal} ref={dialogRef} aria-labelledby="modal-title">
         <div className={styles.modalContainer}>
+          <h2 id="modal-title">{title}</h2>
           {children}
           <button type="button" onClick={() => dialogRef.current.close()}>
             {closeButtonText}
