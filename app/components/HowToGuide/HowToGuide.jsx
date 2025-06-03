8a26586ef0ecc78e30cd90d6e9db0ce840c7c2cd
Ryan <ryanshaunsmith@gmail.com>
2025-06-03 16:43:54 +0100

    update info modal

--- app/components/HowToGuide/HowToGuide.jsx
+++ app/components/HowToGuide/HowToGuide.jsx
@@ -10,12 +10,22 @@ export default function HowToGuide() {
         <p>Here&apos;s a quick guide:</p>
         <ul>
           <li>Start on the Dashboard to select or add a module.</li>
-          <li>Click a module to view and add your learnings.</li>
+          <li>
+            Click a module to:
+            <ul>
+              <li>view and add your learnings.</li>
+              <li>edit your module info.</li>
+              <li>delete the module.</li>
+            </ul>
+          </li>
           <li>
             Use the &quot;Add a new learning&quot; button to document what you&apos;ve learned.
           </li>
+          <li>Click on a learning pill to see more info.</li>
+          <li>You can edit and delete it here as well.</li>
           <li>Use this Help icon anytime to revisit guidance.</li>
         </ul>
+        <small>I'll make this look less awful soon...</small>
       </Modal>
     </div>
   );
