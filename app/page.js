fbf67d2736025b0f579dec121927e2bd4b20269e
Ryan <ryanshaunsmith@gmail.com>
2025-06-04 13:03:39 +0100

    update landing page stylings to be inherently responsive

--- app/page.js
+++ app/page.js
@@ -11,29 +11,31 @@ export default function LandingPage() {
   const { isAuthenticated } = useContext(AuthContext);
   return (
     <div className={styles.container}>
-      <section className={styles.heroMessage}>
-        <h1>OWN YOUR LEARNING JOURNEY</h1>
-        <p className={styles.description}>
-          Learning fast doesn’t mean rushing. <br />
-          Pause. Reflect. Plan. Act.
-          <br />
-          Take control of your progress with intention.
-        </p>
-        {!isAuthenticated ? (
-          <Link href="/routes/auth">
-            <button type="button">Start your Learning Audit now</button>
-          </Link>
-        ) : null}
-      </section>
-      <section aria-label="landing page image" className={styles.heroImgContainer}>
-        <Image
-          className={styles.heroImg}
-          src={heroImage}
-          width={500}
-          height={500}
-          alt="image of a person sitting down to plan their learning"
-        />
-      </section>
+      <div className={styles.containerContent}>
+        <section className={styles.heroMessage}>
+          <h1>OWN YOUR LEARNING JOURNEY</h1>
+          <p className={styles.description}>
+            Learning fast doesn’t mean rushing. <br />
+            Pause. Reflect. Plan. Act.
+            <br />
+            Take control of your progress with intention.
+          </p>
+          {!isAuthenticated ? (
+            <Link href="/routes/auth">
+              <button type="button">Start your Learning Audit now</button>
+            </Link>
+          ) : null}
+        </section>
+        <section aria-label="landing page image" className={styles.heroImgContainer}>
+          <Image
+            className={styles.heroImg}
+            src={heroImage}
+            width={500}
+            height={500}
+            alt="image of a person sitting down to plan their learning"
+          />
+        </section>
+      </div>
     </div>
   );
 }
