0fd29c5cd3084412e58bf945de5811e91ef28145
Ryan <ryanshaunsmith@gmail.com>
2025-06-14 15:29:11 +0100

    destructure and use the correct authLoading state

--- app/routes/ProtectedRoute.jsx
+++ app/routes/ProtectedRoute.jsx
@@ -5,21 +5,21 @@ import { AuthContext } from "../context/authContext";
 import Spinner from "../components/Spinner/Spinner";
 
 export default function ProtectedRoute({ children }) {
-  const { isAuthenticated, isAuthLoading } = useContext(AuthContext);
+  const { isAuthenticated, authLoading } = useContext(AuthContext);
   const router = useRouter();
   const [authChecked, setAuthChecked] = useState(false);
 
   useEffect(() => {
-    if (!isAuthLoading) {
+    if (!authLoading) {
       if (!isAuthenticated) {
         router.push("/");
       } else {
         setAuthChecked(true);
       }
     }
-  }, [isAuthenticated, isAuthLoading, router]);
+  }, [isAuthenticated, authLoading, router]);
 
-  return isAuthLoading || !authChecked ? (
+  return authLoading || !authChecked ? (
     <div style={{ width: "4rem", height: "4rem", margin: "auto" }}>
       <Spinner />
     </div>
